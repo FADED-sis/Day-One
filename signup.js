0c1ae7cb9427b898793f4ab717a54861cef1202c
FADED-sis <mahletgetinet17@gmail.com>
2025-10-29 13:30:47 +0300

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,5 @@
+alert("Welcome To Sign-up Page!");
+document.getElementById("signupBtn").onclick = function(){
+    alert("Congratulation! You have signed in.");
+}
+button.addEventListner('click', e=> {lable.textContent='Congrats, you have signed up!'});
